d712a325b38f2362ab63cdd3c69664fbe34d7969
Roman Chernetsky <misteryo.wins@gmail.com>
2025-05-01 14:18:42 +0300

    realtime forecast, 5 day forecast is on its way

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from 'react'
 import './App.css'
 import './index.css'
 
@@ -8,42 +8,79 @@ function App() {
 
   const [weatherData, setWeatherData] = useState(null)
 
-  const [city, setCity] = useState('London')
+  const [city, setCity] = useState('london')
+
+  const [forecast, setForecast] = useState([])
+
+  useEffect(() => {
+    const fetchWeatherData = async (cityName) => {
+      try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=imperial`;
+        const response = await fetch(url)
+        const data = await response.json()
+        setWeatherData(data)
+        console.log(data);
+
+        const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&units=imperial`)
+        
+        const forecastData = await response.json(forecastResponse)
+
+        const dailyForecast = forecastData.list.filter(
+          (item, index) => index % 8 === 0 // выбирается запись каждые 3 часа -> 8 записей в день -> 5 дней
+        ) 
+        setForecast(dailyForecast)
+      } 
+      catch (err) {
+        console.log(err.message)
+      }
+    }
+    fetchWeatherData(city)
+
+  },[city])
 
   return (
     <div className="wrapper">
-      <div className="header">
-        <h1 className="city">London</h1>
-        <p className="temperature">60°F</p>
-        <p className="condition">Cloudy</p>
-      </div>
-      <div className="weather-details">
-        <div>
-          <p>Humidity</p>
-          <p> 60%</p>
+      {weatherData && weatherData.main && weatherData.weather && (
+    <>
+        <div className="header">
+          <h1 className="city">{weatherData.name}</h1>
+          <p className="temperature">{Math.round(weatherData.main.temp)}°F</p>
+          <p className="condition">{weatherData.weather[0].main}</p>
         </div>
-        <div>
-          <p>Wind Speed</p>
-          <p>7 mph</p>
+        <div className="weather-details">
+          <div>
+            <p>Humidity</p>
+            <p style={{fontWeight:"bold"}}>{Math.round(weatherData.main.humidity)}%</p>
+          </div>
+          <div>
+            <p>Wind Speed</p>
+            <p style={{fontWeight:"bold"}}>{Math.round(weatherData.wind.speed)} mph</p>
+          </div>
         </div>
-      </div>
+      </>
+    )}
+      {forecast.length > 0 && (
+      <>
       <div className="forecast">
         <h2 className="forecast-header">5-Day Forecast</h2>
         <div className="forecast-days">
-          <div className="forecast-day">
-            <p>Monday</p>
-            <p>Cloudy</p>
-            <p>12°F</p>
-          </div>
-          <div className="forecast-day">
-            <p>Monday</p>
-            <p>Cloudy</p>
-            <p>12°F</p>
-          </div>
+          {forecast.map((item, index) => (
+            <div key={index} className="forecast-day">
+              <p>
+                {new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                  weekday: "short",
+                })}
+              </p>
+              <img
+              src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+              alt={item.weather[0].description}
+            />
+            </div>
+          ))}
         </div>
       </div>
+      </>)}
     </div>
-  )
-}
+    )} 
 
 export default App
